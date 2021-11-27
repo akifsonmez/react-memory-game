7802904ca0a1f664a4e3436701922c1e27b925d8
akifsonmez <akifsonmez@outlook.com>
2021-11-27 16:20:46 +0300

    card shuffling added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import './App.css'
-import React from "react"
+import React, { useState } from "react"
+
+const cardImages = [
+  { src: ".public/img/helmet-1.png" },
+  { src: ".public/img/potion-1.png" },
+  { src: ".public/img/ring-1.png" },
+  { src: ".public/img/scroll-1.png" },
+  { src: ".public/img/shield-1.png" },
+  { src: ".public/img/sword-1.png" },
+]
+
 
 function App() {
+
+  const [cards, setCards] = useState([])
+  const [turnCount, setTurnCount] = useState(0)
+
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => { return { ...card, id: Math.random() } })
+    setCards(shuffledCards)
+    setTurnCount(0)
+    }
+
+  console.log(cards, turnCount)
+
   return (
     <div className="App">
       <h1>Magic Match</h1>
-      <button>New Game</button>
+      <button onClick={shuffleCards}>New Game</button>
     </div>
   );
 }
